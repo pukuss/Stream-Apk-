83646655faf3485dd0b4fd89d7fe1856f1e64453
prashanta mondal <prashantaa@gmail.com>
2026-06-13 22:25:32 +0530

    update ui

--- EnfenEra/src/Components/Page/GameProfilePage.jsx
+++ EnfenEra/src/Components/Page/GameProfilePage.jsx
@@ -3,7 +3,7 @@ import {Link, useNavigate, useParams} from 'react-router-dom'
 import LoadingScreen from '../common/LoadingScreen';
 
 import WebLogo from '../horizon/WebLogo'
-import { ArrowLeft, BadgeIndianRupeeIcon, CarTaxiFront, DonutIcon, IndianRupee, LeafyGreen, Menu, ShoppingBag, TruckElectric, Verified } from 'lucide-react';
+import { ArrowLeft, BadgeIndianRupeeIcon, Bubbles, CarTaxiFront, ChevronUp, DonutIcon, Gamepad2, IndianRupee, LeafyGreen, Menu, MoveUpIcon, PlusCircle, ShoppingBag, TruckElectric, Verified } from 'lucide-react';
 
 function GameProfilePage() {
   const gameData = ["bgmi","freefire","callofduty","coc"];
@@ -11,7 +11,7 @@ function GameProfilePage() {
   const {id} = useParams();
 
 
-  const [activeFooter, setActiveFooter] = useState(false);
+  const [activeFooter, setActiveFooter] = useState();
 
   const contact = [
     {
@@ -19,8 +19,18 @@ function GameProfilePage() {
       name : "Call Of Duty Mobile",
       appLogo : "/images/logos/cod.png",
       banner : "/images/pic/cod_background.jpg",
+      appName : "Call Of Duty Mobile",
+      appHost : "Krafton",
+      about : "Call of duty"
+    },
+    {
+      id :2 ,
+      name : "BGMI India",
+      appLogo : "/images/logos/bgmi.png",
+      banner : "/images/pic/bgmi_background.jpg",
       appName : "BGMI (Battlegrounds Mobile India)",
       appHost : "Krafton",
+      about : "Battlegrounds Mobile India"
     },
     {id:2 ,name : "BGMI India", price : 895452},
     {id:3 ,name : "Free Fire India", price : 895452},
@@ -32,6 +42,7 @@ function GameProfilePage() {
   return (
     <>
       <div className='w-full h-full relative'>
+        <div className='absolute top-0 w-full shadow-[10px_10px_500px_50px] shadow-blue-500 '></div>
 
       
         <div className='h-full w-full pb-10   relative'>
@@ -47,7 +58,7 @@ function GameProfilePage() {
             </div>
           </header>
           
-          <div className='max-w-350 m-auto  px-5 py-2 flex bg-slate-950 rounded-xl border border-slate-900'>
+          <div className='max-w-350 m-auto  px-5 py-2 flex bg-gray-950 rounded-xl border border-slate-900'>
             <div className='h-20 flex w-full items-center'>
               <div className='flex '><img className='flex h-20' src={itemlist.appLogo} alt="" /></div>
               <div className='px-3 flex flex-col justify-between  h-full '> 
@@ -78,15 +89,27 @@ function GameProfilePage() {
         <main className='max-full m-auto h-full md:px-10 px-5 bg-gray-700/30 rounded-t-2xl border-gray-500 border py-5'>
         <div>
           <h1 className='font-semibold uppercase '>{itemlist.name}</h1>
-          
-          <h1 className='py-5'> Join Today Now <span className=' text-sm rounded-4xl bg-linear-to-bl from-blue-500 to-blue-700 px-5 py-2'>Enter Your Player Id Or List</span></h1>
+          <h1 className='flex items-center gap-3 text-[12px]'><Bubbles size={15 } /> <p>Importaint Notice : Only For Applicable Indian Reagion Players</p></h1>
+
+          <h1 className='py-5 flex items-center gap-3 font-medium'> <Gamepad2 color='gray' /> Join Today Now <span className='flex  items-center gap-3 text-sm rounded-4xl bg-linear-to-bl from-blue-500 to-blue-700 px-5 font-semibold py-2'><PlusCircle size={20 } /> Enter Your Player Id Or List</span></h1>
+        
+          <div className='w-full  '>
+            <h1 className='flex uppercase font-bold text-gray-500 '>description</h1>
+            <h2 className='flex  pt-5 pb-3 font-bold text-xl'>About {itemlist.name} Game</h2>
+
+            <p className='font-semibold'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam voluptas provident vitae soluta commodi aut obcaecati fugit similique pariatur et suscipit quod sequi ducimus reprehenderit voluptatem, maiores explicabo. Explicabo, cum.</p>
+          </div>
+
+          <div>
+            
+          </div>
         </div>
         </main>
 
 
-        <footer className='bg-gray-800/50 fixed bottom-0 w-full border border-gray-700 rounded-t-2xl py-5 px-5'>
+        <footer className='bg-gray-800/50 fixed bottom-0 w-full border border-gray-700 rounded-t-2xl py-2 px-5'>
           <div className='flex'>
-            Footer
+            <div className='text-[10px] m-auto  flex items-center gap-3 border-b-3 px-3 border-gray-500'> <ChevronUp color='gray' size={15} /></div>
           </div>
           <div>
             {activeFooter && (
